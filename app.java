82674760dfb5df1cbf1a90016836560ae503d39d
taye92 <gmarquil@yahoo.com>
2023-04-16 21:39:23 -0400

    modified

--- app.java
+++ app.java
@@ -3,3 +3,8 @@ developing java apps
 .net applications
 NodeJS applications
 bug removed
+=======
+great
+This app is excellent
+This app is excellent for all users
+>>>>>>> 2a129a9 (excellent app for all)
